0dc4f7a7dd82248d1e67284e774a0a6c0a353007
Fateful-am <fateful.am@gmail.com>
2019-09-30 10:32:03 +0300

    Добавлен скрипт для переключения промо-слайдеров

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 var searchWrapper = document.getElementsByClassName('b-header__search-wrapper')[0];
 var searchButton  = document.getElementsByClassName('b-header__search-button')[0];
 
-function toggleElement( element, classToogle ) {
+var promoSlides = document.getElementsByClassName('b-promo')
+var promoButtons = document.getElementsByClassName('b-promo__slide-button')
+
+function toggleElement(element, classToggle) {
   element.addEventListener('click', function() {
-    this.classList.toggle(classToogle);
+    this.classList.toggle(classToggle);
   });
 }
 
+function slideSwitch(element, butClassToggle, slideClassToggle, btns, slides) {
+  element.addEventListener('click', function() {
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].classList.toggle(butClassToggle, false);
+      slides[i].classList.toggle(slideClassToggle, true);
+      if (this === btns[i]) {
+        btns[i].classList.toggle(butClassToggle);
+        slides[i].classList.toggle(slideClassToggle, false);
+      }
+    }
+    this.classList.toggle(classToggle);
+  });
+}
+
+function toggleSlides(butList, promoList, btns, slides) {
+  for (var i = 0; i < butList.length; i++) {
+    slideSwitch(butList[i], 'b-promo__slide-button--active', 'b-promo--hide', btns, slides);
+  }
+}
+
 toggleElement(searchWrapper, 'b-header__search-wrapper--state-active' );
+toggleSlides(promoButtons, promoSlides, promoButtons, promoSlides);
